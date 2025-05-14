fc2f442ab7fade282391102037abb67792c2955b
Cookieorange735 <hbpschc0000@gmail.com>
2025-05-15 01:23:45 +0800

    nodemen and todo list
    plus nodemen and todo list can go back page

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Node-BackEnd-Practice-
+# Node-BackEnd-Practice
+
--- server.js
+++ server.js
@@ -4,9 +4,15 @@ const app = express();
 
 const port = 5001;
 
+const MyList = [
+    { CTM_map: 1, name: 'Ragecraft IV' },
+    { CTM_map: 2, name: 'Ragecraft III' },
+    { CTM_map: 3, name: 'Ragecraft II' },
+]
+
 const todos = [
-    'todo1', 'todo2', 'todo3'
-];
+  'first todo', 'second todo', 'third todo'
+]
 
 app.set('view engine', 'ejs'); // set view engine ejs
 
@@ -14,29 +20,34 @@ app.get('/', (req, res) => {
     res.send('index');
 })
 
-app.get('/todos', (req, res) => {
-    res.render('todos', {
-        todos: todos,
-    });
-    res.render('What why')
-})
-
 app.get('/test', (req, res) => {
     res.render('test');
 })
 
+app.get('/Mylist',(req, res) => {
+    res.render('Mylist', {
+        MyList: MyList,
+    })
+})
 
 app.get('/hello', (req, res) => {
     res.render('hello');
 })
 
+app.get('/todos', (req, res) => {
+    // 第二個參數可傳入資料
+    res.render('todos', {
+        todos     // todos: todos 一樣的話可省略寫法
+    })
+})
+
 app.get('/todos/:id', (req, res) => {
+  // params: 可拿到網址列上指定的參數
     const id = req.params.id
     const todo = todos[id]
     res.render('todo', {
-        todo: todo,
+        todo,
     })
-    res.render('What why')
 })
 
 app.listen(port, () => {
